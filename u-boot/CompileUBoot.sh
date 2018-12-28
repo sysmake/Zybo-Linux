be46115c170c768d414e42888a0def4457bad6d5
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2018-12-28 16:53:41 +0100

    Fix typo

--- u-boot/CompileUBoot.sh
+++ u-boot/CompileUBoot.sh
@@ -6,7 +6,7 @@ make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} zynq_zybo_config
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 
 # Copy u-boot and configuration
-cp u-boot ../../build/u-boot.elf
+cp u-boot ${DIR_RPOJECT}/build/u-boot.elf
 cp ../uEnv.txt ${DIR_RPOJECT}/build/uEnv.txt
 
 cd ${DIR_RPOJECT}
